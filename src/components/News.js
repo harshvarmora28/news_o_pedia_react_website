ed5a5ec2a762de161f7c40bf6640fc567e5b3f52
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-08-29 14:45:08 +0530

    created next and previous pages

--- src/components/News.js
+++ src/components/News.js
@@ -4,18 +4,50 @@ import NewsItem from './NewsItem'
 const News = () => {
     let articles = []
     const [article, setArticle] = useState(articles);
+    const [page, setPage] = useState(1);
+    const [totalResult, setTotalResult] = useState();
 
     useEffect(() => {
         async function fetchData() {
-            let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=589ccc3f4d9c4617be3bcedea98d1864";
+            let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=1&pageSize=20";
             let data = await fetch(url);
             let parsedData = await data.json();
             setArticle(parsedData.articles);
+            setTotalResult(parsedData.totalResults);
+            
           
         }
         fetchData();
       }, []); // Or [] if effect doesn't need props or state
     
+    const handlePrevClick = () => {
+        async function fetchData() {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=${page - 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            setArticle(parsedData.articles);
+            setPage(page - 1);
+          
+        }
+        fetchData();
+    }  
+    const handleNextClick = () => {
+        if(page + 1 > Math.ceil(totalResult/20)){
+            
+        }
+        else{
+            async function fetchData() {
+                let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=${page + 1}&pageSize=20`;
+                let data = await fetch(url);
+                let parsedData = await data.json();
+                setArticle(parsedData.articles);
+                setPage(page + 1);
+            
+            }
+            fetchData();
+        }
+    }  
+
     return (
         <div className="container my-3">
             <h4>Top Headlines</h4>
@@ -25,6 +57,10 @@ const News = () => {
                     <NewsItem title={element.title?element.title.slice(0, 44):""} description={element.description?element.description.slice(0, 94):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
                     </div> 
                 })}
+            </div>
+            <div className="container d-flex justify-content-between my-4">
+            <button type="button" onClick={handlePrevClick}className="btn btn-dark">&larr; Previous</button>
+            <button type="button" onClick={handleNextClick} className="btn btn-dark">Next &rarr;</button>
             </div>
         </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ const NewsItem = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{props.title}...</h5>
                     <p className="card-text">{props.description}...</p>
-                    <a href={props.newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    <a rel="noreferrer" href={props.newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
                 </div>
                 </div>
         </div>
