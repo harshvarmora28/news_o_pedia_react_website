1424ccecd26e5bdb6ed52e56e87a2a0b36203fad
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-08-29 12:10:53 +0530

    rendered news in first page

--- src/components/News.js
+++ src/components/News.js
@@ -1,48 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NewsItem from './NewsItem'
 
 const News = () => {
-    let articles = [
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
+    let articles = []
     const [article, setArticle] = useState(articles);
+
+    useEffect(() => {
+        async function fetchData() {
+            let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=589ccc3f4d9c4617be3bcedea98d1864";
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            setArticle(parsedData.articles);
+          
+        }
+        fetchData();
+      }, []); // Or [] if effect doesn't need props or state
     
     return (
         <div className="container my-3">
             <h4>Top Headlines</h4>
             <div className="row">
-                {articles.map((element) => {
+                {article.map((element) => {
                     return <div className="col-md-4" key={element.url}>
-                    <NewsItem title={element.title.slice(0, 44)} description={element.description.slice(0, 94)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    <NewsItem title={element.title?element.title.slice(0, 44):""} description={element.description?element.description.slice(0, 94):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
                     </div> 
                 })}
-                 
-                 
             </div>
         </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ const NewsItem = (props) => {
     return (
         <div>
             <div className="card" style={{width: "18rem"}}>
-                <img src={props.imageUrl} className="card-img-top" alt="..."/>
+                <img src={props.imageUrl?props.imageUrl:"https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/iog8o7aw3dwnhfje_1630205389.jpeg"} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{props.title}...</h5>
                     <p className="card-text">{props.description}...</p>
