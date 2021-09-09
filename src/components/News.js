f68492b54d6921b5c6c3289d10a7b9f672ba0727
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-09-09 12:57:07 +0530

    trying to implement infinite scroll but problem is still there with the unique key of the components and the same news is rendering, instead of new news

--- src/components/News.js
+++ src/components/News.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const News = (props) => {
     let articles = []
     const [article, setArticle] = useState(articles);
     const [page, setPage] = useState(1);
-    const [totalResult, setTotalResult] = useState();
+    const [totalResult, setTotalResult] = useState(0);
     const [loading, setLoading] = useState(false);
 
 
@@ -20,60 +21,71 @@ const News = (props) => {
             setArticle(parsedData.articles);
             setTotalResult(parsedData.totalResults);
             setLoading(false);
-            
         }
         fetchData();
       }, [props.pageSize, props.country, props.category]); // Or [] if effect doesn't need props or state
-    
-    const handlePrevClick = () => {
-        async function fetchData() {
-            let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=${page - 1}&pageSize=${props.pageSize}`;
-            setLoading(true);
+
+    // const handlePrevClick = () => {
+    //     async function fetchData() {
+    //         let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=${page - 1}&pageSize=${props.pageSize}`;
+    //         setLoading(true);
+    //         let data = await fetch(url);
+    //         let parsedData = await data.json();
+    //         setArticle(parsedData.articles);
+    //         setPage(page - 1);
+    //         setLoading(false);
+          
+    //     }
+    //     fetchData();
+        
+    // }  
+    // const handleNextClick = () => {
+    //     if(page + 1 > Math.ceil(totalResult/props.pageSize)){
+            
+    //     }
+    //     else{
+    //         async function fetchData() {
+    //             let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=${page + 1}&pageSize=${props.pageSize}`;
+    //             setLoading(true);
+    //             let data = await fetch(url);
+    //             let parsedData = await data.json();
+    //             setArticle(parsedData.articles);
+    //             setPage(page + 1);
+    //             setLoading(false);
+            
+    //         }
+    //         fetchData();
+            
+    //     }
+    // }  
+
+    const fetchMoreData = async () => {
+            setPage(page + 1);
+            let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=1&pageSize=${props.pageSize}`;
             let data = await fetch(url);
             let parsedData = await data.json();
-            setArticle(parsedData.articles);
-            setPage(page - 1);
-            setLoading(false);
-          
-        }
-        fetchData();
+            setArticle(article.concat(parsedData.articles));
+            setTotalResult(parsedData.totalResults);
+        };
         
-    }  
-    const handleNextClick = () => {
-        if(page + 1 > Math.ceil(totalResult/props.pageSize)){
-            
-        }
-        else{
-            async function fetchData() {
-                let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=589ccc3f4d9c4617be3bcedea98d1864&page=${page + 1}&pageSize=${props.pageSize}`;
-                setLoading(true);
-                let data = await fetch(url);
-                let parsedData = await data.json();
-                setArticle(parsedData.articles);
-                setPage(page + 1);
-                setLoading(false);
-            
-            }
-            fetchData();
-            
-        }
-    }  
-
     return (
         <div className="container my-3">
             <h2 className="text-center my-3">Top Headlines</h2>
             {loading && <Spinner/>}
-            <div className="row">
-                {!loading && article.map((element) => {
-                    return <div className="col-md-4" key={element.url}>
-                    <NewsItem title={element.title?element.title.slice(0, 44):""} description={element.description?element.description.slice(0, 94):""} author={element.author} date={element.publishedAt} source={element.source.name} imageUrl={element.urlToImage} newsUrl={element.url}/>
-                    </div> 
-                })}
-            </div>
-            <div className="container d-flex justify-content-between my-4">
-            <button disabled={page <= 1} type="button" onClick={handlePrevClick}className="btn btn-dark">&larr; Previous</button>
-            <button disabled={page + 1 > Math.ceil(totalResult/props.pageSize)} type="button" onClick={handleNextClick} className="btn btn-dark">Next &rarr;</button>
-            </div>
+            <InfiniteScroll
+            dataLength = {articles.length}
+            next = {fetchMoreData}
+            hasMore = {articles.length !== totalResult}
+            loader = {<Spinner/>}
+            >
+                <div className="row">
+                    {article.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title.slice(0, 44):""} description={element.description?element.description.slice(0, 94):""} author={element.author} date={element.publishedAt} source={element.source.name} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        </div> 
+                    })}
+                </div>
+            </InfiniteScroll>
         </div>
     )
 }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,8 @@ import loading from '../loading_spinner.gif';
 const Spinner = () => {
     return (
         <div className="text-center">
-            <img style={{width: "40px", height: "auto",position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} src={loading} alt="Loading..." />
+            {/* <img style={{width: "40px", height: "auto",position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} src={loading} alt="Loading..." /> */}
+            <img style={{width: "40px"}} src={loading} alt="Loading..." />
         </div>
     )
 }
