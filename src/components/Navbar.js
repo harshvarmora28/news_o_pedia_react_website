ad2e1e00f833db30ec6eabf2a1551289965689f8
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-09-11 12:36:39 +0530

    started adding custorm css and not completed yet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "../components/css/Navbar.css"
 
 const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg" id="navbar">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">Navbar</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,15 +5,15 @@ const NewsItem = (props) => {
     
     return (
         <div>
-            <div className="card my-2">
-                <img src={props.imageUrl?props.imageUrl:"https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/iog8o7aw3dwnhfje_1630205389.jpeg"} className="card-img-top" alt="..."/>
+            <div className="card my-2" id="news-card">
+                <img src={props.imageUrl?props.imageUrl:"https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/iog8o7aw3dwnhfje_1630205389.jpeg"} className="card-img-top" id="news-img" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{props.title}...<span className="position-absolute text-dark top-0 translate-middle badge rounded-pill bg-warning" style={{fontSize: "10px", left: "91%", boxShadow: "0px 1px 2px black", zIndex: "1"}}>{props.source}
                         <span className="visually-hidden">unread messages</span>
                         </span></h5>
-                    <p className="card-text">{props.description}...</p>
-                    <p className="card-text"><small className="text-muted">By {props.author?props.author:"Unknown"} on {new Date(props.date).toUTCString()} </small></p>
-                    <a rel="noreferrer" href={props.newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    <p className="card-text" id="news-desc">{props.description}...</p>
+                    <p className="card-text"><small id="muted-text">By {props.author?props.author:"Unknown"} on {new Date(props.date).toUTCString()} </small></p>
+                    <a rel="noreferrer" href={props.newsUrl} target="_blank" className="btn btn-sm" id="read-more-btn">Read More</a>
                 </div>
                 </div>
         </div>
